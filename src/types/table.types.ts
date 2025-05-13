fff5f61f2bff005862f6b375bd7673c3fc966b51
Marcaus.Cruz <marcauscruz@gmail.com>
2025-05-13 13:06:31 -0700

    Establish TableData type

---
+++ src/types/table.types.ts
@@ -0,0 +1,10 @@
+import type { DeckOfCards } from './card.types';
+import type { Player } from './player.types';
+
+export type Table = {
+   deck: DeckOfCards;
+   roundIndex: number;
+   pot: number;
+   playerPositions: Record<number, Player>;
+   players: Player[];
+};
