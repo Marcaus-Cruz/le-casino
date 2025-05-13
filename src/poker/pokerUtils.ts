c12a237b21de2858c801a1bcf2889277ef5db3b7
Marcaus.Cruz <marcauscruz@gmail.com>
2025-05-12 18:21:01 -0700

    Implement simple shuffle function

--- src/poker/pokerUtils.ts
+++ src/poker/pokerUtils.ts
@@ -2,3 +2,6 @@
 //   // Implement the logic to determine the winning hand
 //   return 'Player';
 // }
+export function shuffleArray<T>(array: T[]): T[] {
+   return [...array].sort(() => Math.random() - 0.5);
+}
