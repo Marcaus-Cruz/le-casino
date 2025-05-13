061bc7d7e78bf2f813b84b53882c6c0a40161e66
Marcaus.Cruz <marcauscruz@gmail.com>
2025-05-13 16:16:16 -0700

    Create wait function

--- src/poker/pokerUtils.ts
+++ src/poker/pokerUtils.ts
@@ -5,3 +5,7 @@
 export function shuffleArray<T>(array: T[]): T[] {
    return [...array].sort(() => Math.random() - 0.5);
 }
+
+export function wait(ms: number): Promise<void> {
+   return new Promise(resolve => setTimeout(resolve, ms));
+}
