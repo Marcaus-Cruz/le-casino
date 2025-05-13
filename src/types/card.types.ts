c2d8ac85b8591533ecb68e3e836adaca64173ee4
Marcaus.Cruz <marcauscruz@gmail.com>
2025-05-12 18:20:22 -0700

    WIP update card

--- src/types/card.types.ts
+++ src/types/card.types.ts
@@ -1,10 +1,16 @@
-export type Suit = 'Hearts' | 'Diamonds' | 'Clubs' | 'Spades';
-export type Value = '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K' | 'A';
+export const SUITS = ['hearts', 'diamonds', 'clubs', 'spades'] as const;
+export type Suit = (typeof SUITS)[number];
 
-export type Card = {
+export const VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] as const;
+export type Value = (typeof VALUES)[number];
+
+export interface Card {
+   key: string;
    suit: Suit;
    value: Value;
    imageFront: string;
    imageBack: string;
    isFaceUp: boolean;
-};
+}
+
+export type DeckOfCards = Card[];
