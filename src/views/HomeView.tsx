cdb142cf3bf7412fc9c5eb33cc85c93f7773ef5e
Marcaus.Cruz <marcauscruz@gmail.com>
2025-05-23 12:50:44 -0700

    Implement HomeView V0

---
+++ src/views/HomeView.tsx
@@ -0,0 +1,35 @@
+import Table from '../components/Table';
+import { useState } from 'react';
+import type { GameType } from '../types/table.types';
+import { GAME_TYPES } from '../types/table.types';
+
+export default function HomeView() {
+   const [selectedGame, setSelectedGame] = useState<GameType | null>(null);
+   const validGameTypes = ['draw-five'];
+
+   const onGameLeave = () => setSelectedGame(null);
+
+   return (
+      <div className='view home'>
+         {!selectedGame && <div className='caption'>Select a Game</div>}
+         {!selectedGame && (
+            <div className='game-container'>
+               {Object.entries(GAME_TYPES).map(([btnText, gameType]) => {
+                  return (
+                     <button
+                        key={gameType}
+                        className='btn'
+                        onClick={() => setSelectedGame(gameType)}
+                        disabled={!validGameTypes.includes(gameType)}
+                     >
+                        {btnText}
+                     </button>
+                  );
+               })}
+            </div>
+         )}
+
+         {!!selectedGame && <Table selectedGame={selectedGame} onGameLeave={onGameLeave} />}
+      </div>
+   );
+}
